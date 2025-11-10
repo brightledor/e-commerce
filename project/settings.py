866ce84074ff81093ef7a2d4efdccfef35a2a414
brightledor <bssidd230209@mug.edu.gh>
2025-11-10 13:27:57 -0800

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0yi5h2m77*nso1#tpvq#*&w4ebj*(_h5$szxx0hc7($7xzp*^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["e-commerce-1lwr.onrender.com"]
+ALLOWED_HOSTS = ['e-commerce-1lwr.onrender.com','.onrender.com']
 
 
 # Application definition
